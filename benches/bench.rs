68373d47809bfd44726fc419d04b37830f42ae8b
jerome-trc <circus.of.rats@outlook.com>
2022-07-24 20:09:50 -0400

    Add a criterion benchmark over `VirtualFs::mount_gamedata()`

---
+++ benches/bench.rs
@@ -0,0 +1,30 @@
+use std::{env, path::PathBuf};
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use impure::vfs::{ImpureVfs, VirtualFs};
+
+pub fn vfs(crit: &mut Criterion) {
+	let mut group = crit.benchmark_group("Mount");
+
+	group.sample_size(10);
+
+	group
+		.bench_function("Gamedata", |b| {
+			b.iter(|| {
+				let pwd_gd: PathBuf = [
+					env::current_dir().expect("Failed to retrieve PWD."),
+					PathBuf::from("gamedata"),
+				]
+				.iter()
+				.collect();
+
+				let mut vfs = VirtualFs::default();
+				vfs.mount_gamedata(pwd_gd);
+			});
+		});
+
+	group.finish();
+}
+
+criterion_group!(benches, vfs);
+criterion_main!(benches);
