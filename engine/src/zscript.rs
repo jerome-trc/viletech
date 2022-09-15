2ad20d6ff4e7851d3bb53c8df7642654cee8fb80
jerome-trc <circus.of.rats@outlook.com>
2022-09-14 21:53:38 -0400

    Tidy up imports in engine/src/zscript.rs

--- engine/src/zscript.rs
+++ engine/src/zscript.rs
@@ -21,28 +21,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use crate::vfs::ZsProxyFs;
 
-// use ::zsparse::filesystem::File as ZsFile;
-// use ::zsparse::filesystem::FileSystem as ZsFileSystem;
-use ::zsparse::filesystem::Files as ZsFiles;
-use zsparse::err::ParsingError as ZsParsingError;
-// use zsparse::err::ParsingErrorLevel as ZsParsingErrorLevel;
-use zsparse::parser_manager::parse_filesystem as zs_parse_filesystem;
-use zsparse::parser_manager::FileIndexAndAst as ZsFileIndexAndAst;
+use zsparse::{
+	err::ParsingError,
+	filesystem::Files,
+	parser_manager::{parse_filesystem, FileIndexAndAst},
+};
 
 pub struct ParseOutput {
-	pub files: ZsFiles,
-	pub errors: Vec<ZsParsingError>,
-	pub asts: Vec<ZsFileIndexAndAst>
+	pub files: Files,
+	pub errors: Vec<ParsingError>,
+	pub asts: Vec<FileIndexAndAst>,
 }
 
 pub fn parse(vfs: ZsProxyFs) -> ParseOutput {
 	let mut errors = Default::default();
 	let mut files = Default::default();
-	let asts = zs_parse_filesystem(vfs, &mut files, &mut errors).asts;
+	let asts = parse_filesystem(vfs, &mut files, &mut errors).asts;
 
 	ParseOutput {
 		files,
 		errors,
-		asts
+		asts,
 	}
 }
