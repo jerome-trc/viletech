bda860d3332a53c103cfd1e7eb22c7c2e4eb24fc
jerome-trc <circus.of.rats@outlook.com>
2022-10-28 17:26:17 -0400

    Merge serpent docs with definitions, disable diagnostics

--- data/lua/serpent.lua
+++ data/lua/serpent.lua
@@ -26,6 +26,11 @@ THE SOFTWARE.
 
 --]]
 
+--- @diagnostic disable:redundant-parameter
+--- @diagnostic disable:redundant-return-value
+--- @diagnostic disable:undefined-field
+--- @diagnostic disable:undefined-global
+
 local n, v = "serpent", "0.30" -- (C) 2012-17 Paul Kulchenko; MIT License
 local c, d = "Paul Kulchenko", "Lua serializer and pretty printer"
 local snum = {[tostring(1/0)]='1/0 --[[math.huge]]',[tostring(-1/0)]='-1/0 --[[-math.huge]]',[tostring(0/0)]='0/0'}
@@ -40,6 +45,9 @@ for k,v in pairs(G) do globals[v] = k end -- build func to name mapping
 for _,g in ipairs({'coroutine', 'debug', 'io', 'math', 'string', 'table', 'os'}) do
   for k,v in pairs(type(G[g]) == 'table' and G[g] or {}) do globals[v] = g..'.'..k end end
 
+--- @param t any
+--- @param opts serpent_options
+--- @return string
 local function s(t, opts)
   local name, indent, fatal, maxnum = opts.name, opts.indent, opts.fatal, opts.maxnum
   local sparse, custom, huge = opts.sparse, opts.custom, not opts.nohuge
@@ -147,6 +155,9 @@ local function s(t, opts)
   return not name and body..warn or "do local "..body..sepr..tail.."return "..name..sepr.."end"
 end
 
+--- @param data string The output of a call to `serialize`.
+--- @param opts serpent_options The same options passed to `serialize` to generate `data`.
+--- @return any
 local function deserialize(data, opts)
   local env = (opts and opts.safe == false) and G
     or setmetatable({}, {
@@ -161,8 +172,46 @@ local function deserialize(data, opts)
 end
 
 local function merge(a, b) if b then for k,v in pairs(b) do a[k] = v end end; return a; end
-return { _NAME = n, _COPYRIGHT = c, _DESCRIPTION = d, _VERSION = v, serialize = s,
+
+--- @class serpent_options
+--- @field indent string Indentation; triggers long multi-line output.
+--- @field comment boolean|integer Provide stringified value in a comment (up to `maxlevel` of depth).
+--- @field sortkeys boolean|function Sort keys.
+--- @field sparse boolean Force sparse encoding (no nil filling based on `#t`).
+--- @field compact boolean Remove spaces.
+--- @field fatal boolean Raise fatal error on non-serializable values.
+--- @field fixradix boolean Change radix character set depending on locale to decimal dot.
+--- @field nocode boolean Disable bytecode serialization for easy comparison.
+--- @field nohuge boolean Disable checking numbers against undefined and huge values.
+--- @field maxlevel number Specify max level up to which to expand nested tables.
+--- @field maxnum number Specify max number of elements in a table.
+--- @field maxlength number Specify max length for all table elements.
+--- @field metatostring boolean Use `__tostring` metamethod when serializing tables; set to `false` to disable and serialize the table as-is, even when `__tostring` is present.
+--- @field numformat string e.g. `"%.17g"`. Specify format for numberic values as shortest-possible round-trippable double. Use `"%.16g"` for better readability and `"%.17g"` (the default) to preserve floating point precision.
+--- @field valignore table Allows specifying a list of values (as argument's keys) to ignore.
+--- @field keyallow table Allows specifying the list of keys (as argument's keys) to be serialized. Any keys not in this list are not included in the final output.
+--- @field keyignore table Allows specifying the list of keys to ignore during serialization.
+--- @field custom function Provide custom output for tables.
+--- @field name string Triggers full serialization with self-ref section.
+
+--- @class serpent
+return {
+  _NAME = n, _COPYRIGHT = c, _DESCRIPTION = d, _VERSION = v,
+  serialize = s,
   load = deserialize,
+  --- Settings preset for `serialize`. Sets `compact` and `sparse` to true.
+	--- @param a any
+	--- @param opts serpent_options
+	--- @return string
   dump = function(a, opts) return s(a, merge({name = '_', compact = true, sparse = true}, opts)) end,
+  --- Settings preset for `serialize`. Sets `sortkeys` and `comment` to `true`.
+	--- @param a any
+	--- @param opts serpent_options
+	--- @return string
   line = function(a, opts) return s(a, merge({sortkeys = true, comment = true}, opts)) end,
-  block = function(a, opts) return s(a, merge({indent = '  ', sortkeys = true, comment = true}, opts)) end }
+  --- Settings preset for `serialize`. Sets `sortkeys` and `comment` to `true` and `indent` to `' '`.
+	--- @param a any
+	--- @param opts serpent_options
+	--- @return string
+  block = function(a, opts) return s(a, merge({indent = '  ', sortkeys = true, comment = true}, opts)) end
+}
