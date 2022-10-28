39826f221de8301149d99a643224337189b5f91b
jerome-trc <circus.of.rats@outlook.com>
2022-10-28 15:52:05 -0400

    Add `@nodiscard` annotation to some Lua native function docs

--- lua/library/impure.lua
+++ lua/library/impure.lua
@@ -2,6 +2,7 @@
 
 --- @param path string The virtual file system path to a Lua file.
 --- @return any module If the Lua module compiles successfully and returns anything, that will be returned. Otherwise, returns `nil`.
+--- @nodiscard
 function import(path) end
 
 --- @class impure
@@ -10,6 +11,7 @@ impure = {
 	--- @return number major
 	--- @return number minor
 	--- @return number revision
+	--- @nodiscard
 	version = function() end,
 
 	--- @param msg string A Rust-like format string (only supports `{}`). Gets prefixed with `[INFO]`.
