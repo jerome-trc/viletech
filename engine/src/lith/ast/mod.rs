d78437dd59385974480a9ad2a0047ef317475607
jerome-trc <circus.of.rats@outlook.com>
2022-12-05 20:40:45 -0500

    Lith: parsing, part 5. AST, resolver nodes

--- engine/src/lith/ast/mod.rs
+++ engine/src/lith/ast/mod.rs
@@ -22,8 +22,36 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 mod literal;
 
 use serde::Serialize;
+use vec1::Vec1;
+
+use crate::utils::lang::{FileSpan, Identifier};
 
 #[derive(Debug, Clone, PartialEq, Serialize)]
-pub struct AbstractSyntaxTree {
-	// ???
+pub struct AbstractSyntaxTree<'inp> {
+	pub resolvers: Vec<Resolver<'inp>>,
+}
+
+/// A "resolver" is a double-colon-separated token chain, named after the
+/// concept of "scope resolution". These are the Lith counterpart to Rust "paths",
+/// named differently to disambiguate from the filesystem idea of a "path".
+#[derive(Debug, Clone, PartialEq, Eq, Serialize)]
+pub struct Resolver<'inp> {
+	pub span: FileSpan<'inp>,
+	pub outer: bool,
+	pub parts: Vec1<ResolverPart<'inp>>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize)]
+pub struct ResolverPart<'inp> {
+	pub span: FileSpan<'inp>,
+	#[serde(flatten)]
+	pub kind: ResolverPartKind<'inp>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize)]
+#[serde(tag = "kind", content = "data")]
+pub enum ResolverPartKind<'inp> {
+	Identifier(Identifier<'inp>),
+	Super,
+	SelfUppercase,
 }
