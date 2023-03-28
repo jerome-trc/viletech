873a604f5705292cbd623b59010f4847289af9ef
jerome-trc <circus.of.rats@outlook.com>
2023-03-28 09:06:52 -0400

    Lith: missing expression types in the AST

--- engine/src/lith/ast/expr.rs
+++ engine/src/lith/ast/expr.rs
@@ -10,8 +10,10 @@ use super::*;
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Expression {
 	Binary(ExprBinary),
-	Name(Name),
+	Call(ExprCall),
+	Index(ExprIndex),
 	Literal(Literal),
+	Name(Name),
 	Prefix(ExprPrefix),
 	Postfix(ExprPostfix),
 	Ternary(ExprTernary),
@@ -28,10 +30,12 @@ impl AstNode for Expression {
 		matches!(
 			kind,
 			Syn::ExprBinary
+				| Syn::ExprCall | Syn::ExprIndex
 				| Syn::Name | Syn::Literal
 				| Syn::ExprPostfix
 				| Syn::ExprPrefix
 				| Syn::ExprTernary
+				| Syn::ExprType
 		)
 	}
 
@@ -41,8 +45,10 @@ impl AstNode for Expression {
 	{
 		match node.kind() {
 			Syn::ExprBinary => Some(Self::Binary(ExprBinary(node))),
-			Syn::Name => Some(Self::Name(Name(node))),
+			Syn::ExprCall => Some(Self::Call(ExprCall(node))),
+			Syn::ExprIndex => Some(Self::Index(ExprIndex(node))),
 			Syn::Literal => Some(Self::Literal(Literal(node))),
+			Syn::Name => Some(Self::Name(Name(node))),
 			Syn::ExprPostfix => Some(Self::Postfix(ExprPostfix(node))),
 			Syn::ExprPrefix => Some(Self::Prefix(ExprPrefix(node))),
 			Syn::ExprTernary => Some(Self::Ternary(ExprTernary(node))),
@@ -54,8 +60,10 @@ impl AstNode for Expression {
 	fn syntax(&self) -> &SyntaxNode {
 		match self {
 			Self::Binary(inner) => &inner.0,
-			Self::Name(inner) => &inner.0,
+			Self::Call(inner) => &inner.0,
+			Self::Index(inner) => &inner.0,
 			Self::Literal(inner) => &inner.0,
+			Self::Name(inner) => &inner.0,
 			Self::Prefix(inner) => &inner.0,
 			Self::Postfix(inner) => &inner.0,
 			Self::Ternary(inner) => &inner.0,
@@ -74,39 +82,51 @@ impl Expression {
 	}
 }
 
-/// Wrpas a [`Syn::ExprBinary`].
+/// Wraps a node tagged [`Syn::ExprBinary`].
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[repr(transparent)]
 pub struct ExprBinary(SyntaxNode);
 
 simple_astnode!(Syn, ExprBinary, Syn::ExprBinary);
 
-/// Wraps a [`Syn::ExprPostfix`].
+/// Wraps a node tagged [`Syn::ExprCall`].
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct ExprCall(SyntaxNode);
+
+simple_astnode!(Syn, ExprCall, Syn::ExprCall);
+
+/// Wraps a node tagged [`Syn::ExprIndex`].
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct ExprIndex(SyntaxNode);
+
+simple_astnode!(Syn, ExprIndex, Syn::ExprIndex);
+
+/// Wraps a node tagged [`Syn::ExprPostfix`].
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[repr(transparent)]
 pub struct ExprPostfix(SyntaxNode);
 
 simple_astnode!(Syn, ExprPostfix, Syn::ExprPostfix);
 
-/// Wraps a [`Syn::ExprPrefix`].
+/// Wraps a node tagged [`Syn::ExprPrefix`].
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[repr(transparent)]
 pub struct ExprPrefix(SyntaxNode);
 
 simple_astnode!(Syn, ExprPrefix, Syn::ExprPrefix);
 
-/// Wraps a [`Syn::ExprTernary`].
+/// Wraps a node tagged [`Syn::ExprTernary`].
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[repr(transparent)]
 pub struct ExprTernary(SyntaxNode);
 
 simple_astnode!(Syn, ExprTernary, Syn::ExprTernary);
 
-/// Wraps a [`Syn::ExprType`].
+/// Wraps a node tagged [`Syn::ExprType`].
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[repr(transparent)]
 pub struct ExprType(SyntaxNode);
 
 simple_astnode!(Syn, ExprType, Syn::ExprType);
-
-impl ExprType {}
