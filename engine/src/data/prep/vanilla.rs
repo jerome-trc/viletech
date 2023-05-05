3e876c005168379c5cfc817586cb6abba6ebc7d5
jerome-trc <circus.of.rats@outlook.com>
2023-05-04 20:11:30 -0400

    engine/src/data/prep/wad.rs and engine/src/data/prep/vanilla.rs
    Reducing clutter in prep.rs, since there's a lot more of this on the way.

---
+++ engine/src/data/prep/vanilla.rs
@@ -0,0 +1,247 @@
+//! Functions for processing formats shipped with the original commercial IWADs.
+
+use std::io::Cursor;
+
+use arrayvec::ArrayVec;
+use bevy::prelude::{IVec2, UVec2};
+use byteorder::{ByteOrder, LittleEndian, ReadBytesExt};
+use image::Rgba;
+
+use crate::{
+	data::{
+		detail::Outcome, prep::read_shortid, vfs::FileRef, AssetHeader, Catalog, Image, Palette,
+		PaletteSet, PrepError, PrepErrorKind,
+	},
+	utils::io::CursorExt,
+	ShortId,
+};
+
+use super::SubContext;
+
+/// See <https://doomwiki.org/wiki/PNAMES>.
+#[derive(Debug)]
+pub(super) struct PatchTable(pub(super) Vec<ShortId>);
+
+/// See <https://doomwiki.org/wiki/TEXTURE1_and_TEXTURE2>.
+#[derive(Debug)]
+pub(super) struct TextureX(pub(super) Vec<PatchedTex>);
+
+#[derive(Debug)]
+pub(super) struct PatchedTex {
+	pub(super) _name: ShortId,
+	pub(super) _size: UVec2,
+	pub(super) _patches: Vec<TexPatch>,
+}
+
+#[derive(Debug)]
+pub(super) struct TexPatch {
+	/// Offset of this patch relative to the upper-left of the whole texture.
+	pub(super) _origin: IVec2,
+	/// Index into [`PatchTable`].
+	pub(super) _index: usize,
+}
+
+impl Catalog {
+	/// Returns `None` to indicate that `bytes` was checked
+	/// and determined to not be a picture.
+	#[must_use]
+	pub(super) fn prep_picture(&self, ctx: &SubContext, bytes: &[u8], id: &str) -> Option<()> {
+		// TODO: Wasteful to run a hash lookup before checking if this is a picture.
+		let palettes = self.last_asset_by_nick::<PaletteSet>("PLAYPAL").unwrap();
+
+		if let Some(image) = Image::try_from_picture(bytes, &palettes.palettes[0]) {
+			ctx.add_asset::<Image>(Image {
+				header: AssetHeader {
+					id: format!("{mount_id}/{id}", mount_id = ctx.mntinfo.id()),
+				},
+				inner: image.0,
+				offset: image.1,
+			});
+
+			Some(())
+		} else {
+			None
+		}
+	}
+
+	pub(super) fn prep_playpal(&self, ctx: &SubContext, bytes: &[u8]) -> std::io::Result<()> {
+		let mut palettes = ArrayVec::<_, 14>::default();
+		let mut cursor = Cursor::new(bytes);
+
+		for _ in 0..14 {
+			let mut pal = Palette::black();
+
+			for ii in 0..256 {
+				let r = cursor.read_u8()?;
+				let g = cursor.read_u8()?;
+				let b = cursor.read_u8()?;
+				pal.0[ii] = Rgba([r, g, b, 255]);
+			}
+
+			palettes.push(pal);
+		}
+
+		ctx.add_asset::<PaletteSet>(PaletteSet {
+			header: AssetHeader {
+				id: format!("{}/PLAYPAL", ctx.mntinfo.id()),
+			},
+			palettes: palettes.into_inner().unwrap(),
+		});
+
+		Ok(())
+	}
+
+	/// Returns `None` if the given PNAMES lump is valid, but reports itself to
+	/// have 0 records in it.
+	pub(super) fn prep_pnames(
+		&self,
+		_ctx: &SubContext,
+		lump: FileRef,
+		bytes: &[u8],
+	) -> Outcome<PatchTable, PrepError> {
+		const RECORD_SIZE: usize = 8;
+
+		let mut invalid = false;
+
+		invalid |= bytes.len() < RECORD_SIZE;
+		invalid |= ((bytes.len() - 4) % RECORD_SIZE) != 0;
+
+		let len = LittleEndian::read_u32(bytes) as usize;
+
+		if len == 0 {
+			return Outcome::None;
+		}
+
+		invalid |= bytes.len() != ((len * RECORD_SIZE) + 4);
+
+		if invalid {
+			return Outcome::Err(PrepError {
+				path: lump.path.to_path_buf(),
+				kind: PrepErrorKind::PNames,
+				fatal: false,
+			});
+		}
+
+		let mut ret = Vec::with_capacity(len);
+		let mut pos = 4;
+
+		while pos < bytes.len() {
+			let raw = bytemuck::from_bytes::<[u8; RECORD_SIZE]>(&bytes[pos..(pos + RECORD_SIZE)]);
+
+			if let Some(pname) = read_shortid(*raw) {
+				ret.push(pname);
+			}
+
+			pos += RECORD_SIZE;
+		}
+
+		Outcome::Ok(PatchTable(ret))
+	}
+
+	pub(super) fn prep_texturex(
+		&self,
+		_ctx: &SubContext,
+		lump: FileRef,
+		bytes: &[u8],
+	) -> Outcome<TextureX, PrepError> {
+		#[repr(C)]
+		#[derive(Debug, Clone, Copy, PartialEq, Eq, bytemuck::AnyBitPattern)]
+		struct RawMapTexture {
+			name: [u8; 8],
+			/// C boolean, unused.
+			masked: i32,
+			width: i16,
+			height: i16,
+			/// Unused.
+			col_dir: i32,
+			patch_count: i16,
+		}
+
+		#[repr(C)]
+		#[derive(Debug, Clone, Copy, PartialEq, Eq, bytemuck::AnyBitPattern)]
+		struct RawMapPatch {
+			origin_x: i16,
+			origin_y: i16,
+			/// Index into [`PatchTable`].
+			patch: i16,
+			/// Unused.
+			stepdir: i16,
+			/// Unused.
+			colormap: i16,
+		}
+
+		let err_fn = || PrepError {
+			path: lump.path.to_path_buf(),
+			kind: PrepErrorKind::TextureX,
+			fatal: false,
+		};
+
+		if bytes.len() < 4 {
+			return Outcome::Err(err_fn());
+		}
+
+		let num_textures = LittleEndian::read_u32(bytes) as usize;
+
+		if num_textures == 0 {
+			return Outcome::None;
+		}
+
+		let mut curs_maptex = Cursor::new(bytes);
+		curs_maptex.set_position(curs_maptex.position() + 4);
+
+		let mut ret = Vec::with_capacity(num_textures);
+
+		for _ in 0..num_textures {
+			let start = curs_maptex.read_i32::<LittleEndian>().unwrap() as usize;
+			let end = start + 22;
+
+			if end > bytes.len() {
+				return Outcome::Err(err_fn());
+			}
+
+			let mut curs_patch = Cursor::new(bytes);
+			curs_patch.set_position(start as u64);
+
+			let raw_tex = RawMapTexture {
+				name: *curs_patch.read_from_bytes::<[u8; 8]>(),
+				masked: curs_patch.read_i32::<LittleEndian>().unwrap(),
+				width: curs_patch.read_i16::<LittleEndian>().unwrap(),
+				height: curs_patch.read_i16::<LittleEndian>().unwrap(),
+				col_dir: curs_patch.read_i32::<LittleEndian>().unwrap(),
+				patch_count: curs_patch.read_i16::<LittleEndian>().unwrap(),
+			};
+
+			let patch_count = raw_tex.patch_count as usize;
+
+			debug_assert_eq!(curs_patch.position(), end as u64);
+
+			let mut patches = Vec::with_capacity(patch_count);
+
+			for _ in 0..patch_count {
+				let end = curs_patch.position() + (std::mem::size_of::<RawMapPatch>() as u64);
+
+				if end as usize > bytes.len() {
+					return Outcome::Err(err_fn());
+				}
+
+				let range = (curs_patch.position() as usize)..(end as usize);
+				let raw_patch = bytemuck::from_bytes::<RawMapPatch>(&bytes[range]);
+
+				patches.push(TexPatch {
+					_origin: glam::ivec2(raw_patch.origin_x as i32, raw_patch.origin_y as i32),
+					_index: raw_patch.patch as usize,
+				});
+
+				curs_patch.set_position(end);
+			}
+
+			ret.push(PatchedTex {
+				_name: read_shortid(raw_tex.name).unwrap_or_default(),
+				_size: glam::uvec2(raw_tex.width as u32, raw_tex.height as u32),
+				_patches: patches,
+			});
+		}
+
+		Outcome::Ok(TextureX(ret))
+	}
+}
---
+++ engine/src/data/prep/wad.rs
@@ -0,0 +1,169 @@
+//! Functions for reading assets from WADs.
+
+use rayon::prelude::*;
+
+use crate::data::{detail::Outcome, vfs::FileRef, Audio, Catalog, PrepError, PrepErrorKind};
+
+use super::SubContext;
+
+impl Catalog {
+	pub(super) fn prep_pass1_wad(&self, _ctx: &SubContext) -> Outcome<(), ()> {
+		// TODO: Soon!
+		Outcome::None
+	}
+
+	pub(super) fn prep_pass2_wad(&self, ctx: &SubContext) {
+		let wad = self.vfs.get(ctx.mntinfo.virtual_path()).unwrap();
+
+		wad.children().unwrap().par_bridge().try_for_each(|child| {
+			if !child.is_readable() {
+				return Some(());
+			}
+
+			if ctx.tracker.is_cancelled() {
+				return None;
+			}
+
+			let bytes = child.read_bytes();
+			let fstem = child.file_prefix();
+
+			if fstem == "PLAYPAL" {
+				match self.prep_playpal(ctx, bytes) {
+					Ok(()) => {}
+					Err(err) => {
+						ctx.errors.lock().push(PrepError {
+							path: child.path().to_path_buf(),
+							kind: PrepErrorKind::Io(err),
+							fatal: false,
+						});
+					}
+				}
+
+				return Some(());
+			}
+
+			if fstem == "PNAMES" {
+				match self.prep_pnames(
+					ctx,
+					FileRef {
+						vfs: &self.vfs,
+						file: child,
+					},
+					bytes,
+				) {
+					Outcome::Ok(pnames) => ctx.artifacts.lock().pnames = Some(pnames),
+					Outcome::Err(err) => ctx.errors.lock().push(err),
+					Outcome::None => {}
+					_ => unreachable!(),
+				}
+
+				return Some(());
+			}
+
+			if fstem == "TEXTURE1" || fstem == "TEXTURE2" {
+				match self.prep_texturex(
+					ctx,
+					FileRef {
+						vfs: &self.vfs,
+						file: child,
+					},
+					bytes,
+				) {
+					Outcome::Ok(texturex) => {
+						if fstem.ends_with('1') {
+							ctx.artifacts.lock().texture1 = Some(texturex);
+						} else if fstem.ends_with('2') {
+							ctx.artifacts.lock().texture2 = Some(texturex);
+						}
+					}
+					Outcome::Err(err) => ctx.errors.lock().push(err),
+					_ => unreachable!(),
+				}
+			}
+
+			Some(())
+		});
+	}
+
+	pub(super) fn prep_pass3_wad(&self, ctx: &SubContext) -> Outcome<(), ()> {
+		let wad = self.vfs.get(ctx.mntinfo.virtual_path()).unwrap();
+
+		let cancelled = wad
+			.child_refs()
+			.unwrap()
+			.filter(|c| !c.is_empty())
+			.par_bridge()
+			.try_for_each(|child| {
+				if ctx.tracker.is_cancelled() {
+					return None;
+				}
+
+				if child.is_dir() {
+					self.prep_pass3_wad_dir(ctx, child);
+				} else {
+					self.prep_pass3_wad_entry(ctx, child);
+				};
+
+				Some(())
+			});
+
+		match cancelled {
+			Some(()) => Outcome::Ok(()),
+			None => Outcome::Cancelled,
+		}
+	}
+
+	fn prep_pass3_wad_entry(&self, ctx: &SubContext, vfile: FileRef) {
+		let bytes = vfile.read_bytes();
+		let fstem = vfile.file_prefix();
+
+		/// Kinds of WAD entries irrelevant to this pass.
+		const UNHANDLED: &[&str] = &[
+			"COLORMAP", "DMXGUS", "ENDOOM", "GENMIDI", "PLAYPAL", "PNAMES", "TEXTURE1", "TEXTURE2",
+		];
+
+		if UNHANDLED.iter().any(|&name| fstem == name)
+			|| Audio::is_pc_speaker_sound(bytes)
+			|| Audio::is_dmxmus(bytes)
+		{
+			return;
+		}
+
+		ctx.tracker.add_prep_progress(1);
+
+		let is_pic = self.prep_picture(ctx, bytes, fstem);
+
+		// TODO: Processors for more file formats.
+
+		let res: std::io::Result<()> = if is_pic.is_some() {
+			Ok(())
+		} else {
+			return;
+		};
+
+		match res {
+			Ok(()) => {}
+			Err(err) => {
+				ctx.errors.lock().push(PrepError {
+					path: vfile.path().to_path_buf(),
+					kind: PrepErrorKind::Io(err),
+					fatal: true,
+				});
+			}
+		}
+	}
+
+	fn prep_pass3_wad_dir(&self, ctx: &SubContext, dir: FileRef) {
+		match self.try_prep_level_vanilla(ctx, dir) {
+			Outcome::Ok(()) | Outcome::Err(()) => return,
+			Outcome::None => {}
+			_ => unreachable!(),
+		}
+
+		match self.try_prep_level_udmf(ctx, dir) {
+			Outcome::Ok(()) | Outcome::Err(()) => {}
+			Outcome::None => {}
+			_ => unreachable!(),
+		}
+	}
+}
