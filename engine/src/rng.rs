0f23b1e5e89b850f420b1017a95eb057cdc01d8f
jerome-trc <circus.of.rats@outlook.com>
2023-06-02 12:14:03 -0400

    RNG code touchups
    - `Prng` provides a function for shuffling slices, and provides `coin_flip` (now remaned to `boolean`.
    - Required method `Prng::seed`.
    - Remove the unused error type.
    - File re-ordering for better clarity.

--- engine/src/rng.rs
+++ engine/src/rng.rs
@@ -1,82 +1,9 @@
 //! Symbols relating to pseudo-random number generation.
 
-use std::{collections::HashMap, fmt};
+use std::collections::HashMap;
 
 use bevy::prelude::Resource;
-use nanorand::{Rng, WyRand};
-
-#[derive(Debug)]
-pub enum Error {
-	KeyOverlap,
-}
-
-impl std::error::Error for Error {}
-
-impl fmt::Display for Error {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		match self {
-			Self::KeyOverlap => {
-				write!(f, "Tried to insert an RNG under an already-registered key.")
-			}
-		}
-	}
-}
-
-/// The PRNG behavior used by the engine all passes through this trait, so that
-/// alternative implementations (e.g. Boom-like, Doom-like, SFMT) can
-/// hypothetically be built and substituted.
-pub trait Prng: Default {
-	#[must_use]
-	fn range_i64(&mut self, min_incl: i64, max_incl: i64) -> i64;
-	#[must_use]
-	fn range_f64(&mut self, min_incl: f64, max_incl: f64) -> f64;
-	#[must_use]
-	fn range_usize(&mut self, min_incl: usize, max_incl: usize) -> usize;
-	#[must_use]
-	fn coin_flip(&mut self) -> bool;
-
-	/// Returns a random character in the range from 0x61 to 0x7A.
-	#[must_use]
-	fn ascii_lowercase(&mut self) -> char {
-		char::from_u32(self.range_usize(97, 122) as u32).unwrap()
-	}
-
-	/// Returns a random character in the range from 0x41 to 0x5A.
-	#[must_use]
-	fn ascii_uppercase(&mut self) -> char {
-		char::from_u32(self.range_usize(65, 90) as u32).unwrap()
-	}
-
-	/// Returns a random character in the range from 0x30 to 0x39.
-	#[must_use]
-	fn ascii_digit(&mut self) -> char {
-		char::from_u32(self.range_usize(48, 57) as u32).unwrap()
-	}
-
-	/// Returns a random character in the range from 0x30 to 0x39.
-	#[must_use]
-	fn ascii_char(&mut self) -> char {
-		char::from_u32(self.range_usize(33, 126) as u32).unwrap()
-	}
-}
-
-impl Prng for WyRand {
-	fn range_i64(&mut self, min_incl: i64, max_incl: i64) -> i64 {
-		self.generate_range(min_incl..=max_incl)
-	}
-
-	fn range_f64(&mut self, min_incl: f64, max_incl: f64) -> f64 {
-		min_incl + (self.generate::<f64>() / (1.0 / (max_incl - min_incl)))
-	}
-
-	fn range_usize(&mut self, min_incl: usize, max_incl: usize) -> usize {
-		self.generate_range(min_incl..=max_incl)
-	}
-
-	fn coin_flip(&mut self) -> bool {
-		self.generate()
-	}
-}
+use nanorand::{Rng, SeedableRng, WyRand};
 
 /// Contains a map of named random number generators.
 #[derive(Debug, Resource)]
@@ -134,5 +61,86 @@ impl<B: Prng> RngCore<B> {
 	#[must_use]
 	pub fn contains(&self, key: &str) -> bool {
 		self.prngs.contains_key(key)
+	}
+}
+
+// PRNG trait //////////////////////////////////////////////////////////////////
+
+/// The PRNG behavior used by the engine all passes through this trait, so that
+/// alternative implementations (e.g. Boom-like, Doom-like, SFMT) can
+/// hypothetically be built and substituted.
+pub trait Prng: Default {
+	fn seed(&mut self, seed: u64);
+
+	#[must_use]
+	fn range_i64(&mut self, min_incl: i64, max_incl: i64) -> i64;
+	#[must_use]
+	fn range_f64(&mut self, min_incl: f64, max_incl: f64) -> f64;
+	#[must_use]
+	fn range_usize(&mut self, min_incl: usize, max_incl: usize) -> usize;
+
+	// Provided ////////////////////////////////////////////////////////////////
+
+	#[must_use]
+	fn boolean(&mut self) -> bool {
+		self.range_i64(0, 1) == 0
+	}
+
+	/// Returns a random character in the range from 0x61 to 0x7A.
+	#[must_use]
+	fn ascii_lowercase(&mut self) -> char {
+		char::from_u32(self.range_usize(97, 122) as u32).unwrap()
+	}
+
+	/// Returns a random character in the range from 0x41 to 0x5A.
+	#[must_use]
+	fn ascii_uppercase(&mut self) -> char {
+		char::from_u32(self.range_usize(65, 90) as u32).unwrap()
+	}
+
+	/// Returns a random character in the range from 0x30 to 0x39.
+	#[must_use]
+	fn ascii_digit(&mut self) -> char {
+		char::from_u32(self.range_usize(48, 57) as u32).unwrap()
+	}
+
+	/// Returns a random character in the range from 0x30 to 0x39.
+	#[must_use]
+	fn ascii_char(&mut self) -> char {
+		char::from_u32(self.range_usize(33, 126) as u32).unwrap()
+	}
+
+	fn shuffle<T>(&mut self, mut target: impl AsMut<[T]>) {
+		let target = target.as_mut();
+		let len = target.len();
+
+		for i in 0..len {
+			let r = self.range_usize(0, len);
+			target.swap(i, r);
+		}
+	}
+}
+
+// WyRand //////////////////////////////////////////////////////////////////////
+
+impl Prng for WyRand {
+	fn seed(&mut self, seed: u64) {
+		self.reseed(seed.to_le_bytes());
+	}
+
+	fn range_i64(&mut self, min_incl: i64, max_incl: i64) -> i64 {
+		self.generate_range(min_incl..=max_incl)
+	}
+
+	fn range_f64(&mut self, min_incl: f64, max_incl: f64) -> f64 {
+		min_incl + (self.generate::<f64>() / (1.0 / (max_incl - min_incl)))
+	}
+
+	fn range_usize(&mut self, min_incl: usize, max_incl: usize) -> usize {
+		self.generate_range(min_incl..=max_incl)
+	}
+
+	fn boolean(&mut self) -> bool {
+		self.generate()
 	}
 }
