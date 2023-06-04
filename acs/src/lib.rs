5259b5d9fad07b61a12ff90f27198bd2a025e254
jerome-trc <circus.of.rats@outlook.com>
2023-06-04 11:52:37 -0400

    New crate: `viletech-acs`
    An experiment I've been mulling over for a while; ACS JIT compilation. Not sure if this can be made to work. In any case having the object file reader I'll need in a different crate makes sense. Maybe a full compiler can follow in the far future; ACC isn't a lot of code to fork.
    
    Just boilerplate and the p-code enumeration. Won't be migrating over everything in `engine` yet since I don't plan on porting GZDoom's whole interpreter, and I don't know how much of the existing work is thus relevant.

---
+++ acs/src/lib.rs
@@ -0,0 +1,7 @@
+//! # VileTech ACS
+//!
+//! The VTACS toolchain; a [Cranelift] backend for Raven Software's [Action Code Script]
+//! as well as a reader for its object file format.
+//!
+//! [Cranelift]: https://cranelift.dev/
+//! [Action Code Script]: https://doomwiki.org/wiki/ACS
