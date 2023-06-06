c46bbd707bde95aecc22ae2ddd93be0608651d6c
jerome-trc <circus.of.rats@outlook.com>
2023-06-06 17:17:31 -0400

    Subterra: migrate level datum representation; require Rayon
    This feature flag seems like a decent idea until it becomes apparent how much code duplication will be forced due to needing to change iterators, needing to change to atomics, needing to add locks, et cetera. The problem will be less severe in a hypothetical future where all these data structure and algorithms are more stable.
    
    Additionally, bring in `once_cell` and `regex`, needed for the coming UDMF parser migration.

--- level/src/lib.rs
+++ level/src/lib.rs
@@ -3,4 +3,7 @@
 //! Subterra is the code used by the VileTech Engine for reading, storing,
 //! manipulating, and writing Doom levels, whether vanilla or UDMF.
 
-// Soon!
+pub mod repr;
+pub mod udmf;
+
+pub use repr::Level;
