d20d48777931e6480075b45f7b8c4e29384a6ec1
jerome-trc <circus.of.rats@outlook.com>
2023-06-13 22:00:38 -0400

    VZS: make `smoke_func_decl` unit test compile again
    Not passing yet; it won't until expression parsing understands the field access operator.

--- vzscript/src/parse/item.rs
+++ vzscript/src/parse/item.rs
@@ -66,3 +66,45 @@ impl<C: GreenCache> ParserBuilder<C> {
 		.map(|group| coalesce_node(group, Syn::ParamList))
 	}
 }
+
+#[cfg(test)]
+mod test {
+	use doomfront::{gcache::GreenCacheNoop, rowan::ast::AstNode, testing::*};
+
+	use crate::{ast, ParseTree, Version};
+
+	use super::*;
+
+	#[test]
+	fn smoke_func_decl() {
+		const SOURCE: &str = r#"
+func conductor();
+func r#faultline():void{}
+func atmospheric_pressure():vzs.int32;
+func untilted(): type(1) {}
+func liquid_luck(within: reach) {}
+func quicksilver(heliotrope: escape.velocity) {}
+func atomic(r#123: coal.r#yaw);
+"#;
+
+		let vers = Version::new(0, 0, 0);
+		let builder = ParserBuilder::<GreenCacheNoop>::new(vers);
+		let parser = builder.file();
+		let tbuf = doomfront::scan(SOURCE, vers);
+		let result = doomfront::parse(parser, SOURCE, &tbuf);
+		let ptree: ParseTree = unwrap_parse_tree(result);
+
+		assert_no_errors(&ptree);
+
+		let mut ast = ptree
+			.cursor()
+			.children()
+			.map(|root| ast::FileRoot::cast(root).unwrap());
+
+		let fndecl1 = ast.next().unwrap().into_func_decl().unwrap();
+		assert_eq!(fndecl1.name().text(), "conductor");
+
+		let fndecl2 = ast.next().unwrap().into_func_decl().unwrap();
+		assert_eq!(fndecl2.name().text(), "faultline");
+	}
+}
