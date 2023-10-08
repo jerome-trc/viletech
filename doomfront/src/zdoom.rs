941597279a00e968905ea6078710c8f5453d1e21
jerome-trc <circus.of.rats@outlook.com>
2023-10-08 10:46:37 -0400

    DF: `impl std::fmt::Display for zdoom::Version`

--- doomfront/src/zdoom.rs
+++ doomfront/src/zdoom.rs
@@ -90,3 +90,9 @@ impl std::str::FromStr for Version {
 		})
 	}
 }
+
+impl std::fmt::Display for Version {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{}.{}.{}", self.major, self.minor, self.rev)
+	}
+}
