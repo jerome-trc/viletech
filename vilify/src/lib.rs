de02c6af3c4e4f67d65a6e2ed55585ed73bce435
jerome-trc <circus.of.rats@outlook.com>
2023-10-11 13:10:54 -0400

    Vilify: docs logo and favicon

--- vilify/src/lib.rs
+++ vilify/src/lib.rs
@@ -1,1 +1,6 @@
 //! # Vilify
+
+#![doc(
+	html_favicon_url = "https://media.githubusercontent.com/media/jerome-trc/viletech/master/assets/viletech/viletech.png",
+	html_logo_url = "https://media.githubusercontent.com/media/jerome-trc/viletech/master/assets/viletech/viletech.png"
+)]
