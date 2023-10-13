91f5949379f5c5e4ce573b8ebac613e0493a3d83
jerome-trc <circus.of.rats@outlook.com>
2023-10-13 12:14:58 -0400

    Lith: rudimentary file tree builder from physical FS
    Not useful to the engine, but very useful for upcoming unit and integration tests.

--- lith/src/filetree.rs
+++ lith/src/filetree.rs
@@ -1,6 +1,10 @@
-use petgraph::prelude::DiGraph;
+use std::path::Path;
 
-use crate::ParseTree;
+use parking_lot::Mutex;
+use petgraph::prelude::DiGraph;
+use rayon::prelude::*;
+
+use crate::{parse, ParseTree};
 
 pub type FileIx = petgraph::graph::DefaultIx;
 
@@ -8,7 +12,7 @@ pub type FileIx = petgraph::graph::DefaultIx;
 pub struct FileTree {
 	/// Edges run from parents ([`Node::Folder`]) to children ([`Node::File`]).
 	/// An invalid graph is always safe but will cause unexpected compiler errors.
-	pub files: DiGraph<Node, (), FileIx>,
+	pub graph: DiGraph<Node, (), FileIx>,
 }
 
 #[derive(Debug)]
@@ -20,7 +24,7 @@ pub enum Node {
 impl FileTree {
 	#[must_use]
 	pub fn valid(&self) -> bool {
-		for node in self.files.node_weights() {
+		for node in self.graph.node_weights() {
 			let Node::File { ptree, .. } = node else {
 				continue;
 			};
@@ -31,5 +35,83 @@ impl FileTree {
 		}
 
 		true
+	}
+
+	pub fn files(&self) -> impl Iterator<Item = (&String, &ParseTree)> {
+		self.graph.node_weights().filter_map(|ftn| {
+			let Node::File { ptree, path } = ftn else {
+				return None;
+			};
+
+			Some((path, ptree))
+		})
+	}
+
+	/// `root` can be a directory or file.
+	/// Blocks on the [`rayon`] global thread pool for parallelized parsing.
+	pub fn from_fs(root: &Path) -> std::io::Result<Self> {
+		if !root.is_dir() {
+			unimplemented!()
+		}
+
+		let files = Mutex::default();
+		Self::from_fs_recur(&files, root);
+
+		Ok(Self {
+			graph: files.into_inner(),
+		})
+	}
+
+	fn from_fs_recur(files: &Mutex<DiGraph<Node, (), FileIx>>, dir: &Path) {
+		let Ok(dir_iter) = std::fs::read_dir(dir) else {
+			// TODO: accumulate and return errors.
+			return;
+		};
+
+		let dir_ix = files.lock().add_node(Node::Folder {
+			path: dir.to_string_lossy().into_owned(),
+		});
+
+		dir_iter.par_bridge().for_each(|result| {
+			let Ok(dir_entry) = result else {
+				// TODO: accumulate and return errors.
+				return;
+			};
+
+			let path = dir_entry.path();
+
+			if path.is_dir() {
+				Self::from_fs_recur(files, &path);
+				return;
+			}
+
+			if !path
+				.extension()
+				.is_some_and(|ext| ext.eq_ignore_ascii_case("lith"))
+			{
+				return;
+			}
+
+			let Ok(bytes) = std::fs::read(&path) else {
+				// TODO: accumulate and return errors.
+				return;
+			};
+
+			let Ok(text) = String::from_utf8(bytes) else {
+				// TODO: accumulate and return errors.
+				return;
+			};
+
+			let ptree = doomfront::parse(&text, parse::file, ());
+
+			let mut guard = files.lock();
+
+			let file_ix = guard.add_node(Node::File {
+				ptree,
+				path: path.to_string_lossy().into_owned(),
+			});
+
+			guard.add_edge(dir_ix, file_ix, ());
+		});
 	}
 }
