60f14931d628937dcaa29bff416f367cee625f2e
jerome-trc <circus.of.rats@outlook.com>
2023-10-14 15:41:54 -0400

    Lith: benchmark group for Cranelift operations

--- lith/benches/bench.rs
+++ lith/benches/bench.rs
@@ -2,8 +2,26 @@ use append_only_vec::AppendOnlyVec;
 use dashmap::DashMap;
 use util::rstring::RString;
 
-criterion::criterion_group!(benches, string_interning);
+criterion::criterion_group!(benches, cranelift_ops, string_interning);
 criterion::criterion_main!(benches);
+
+fn cranelift_ops(crit: &mut criterion::Criterion) {
+	let mut grp = crit.benchmark_group("Cranelift Ops");
+
+	grp.bench_function("New CLIF Function", |bencher| {
+		bencher.iter(|| {
+			let _ = std::hint::black_box(cranelift::codegen::ir::Function::new());
+		});
+	});
+
+	grp.bench_function("New CLIF Function Context", |bencher| {
+		bencher.iter(|| {
+			let _ = std::hint::black_box(cranelift::codegen::Context::new());
+		});
+	});
+
+	grp.finish();
+}
 
 fn string_interning(crit: &mut criterion::Criterion) {
 	let mut grp = crit.benchmark_group("String Interning");
