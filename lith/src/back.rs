59aeeb5996d0fa853bfeb8a50c1ca27a7146aca8
jerome-trc <circus.of.rats@outlook.com>
2023-10-17 06:56:50 -0400

    Lith: don't put a `Mutex` in `JitModule`
    Give the rest of the crate more flexibility in how to use this structure.

--- lith/src/back.rs
+++ lith/src/back.rs
@@ -3,15 +3,14 @@
 use std::mem::MaybeUninit;
 
 use cranelift_jit::JITModule;
-use parking_lot::Mutex;
 
 /// Newtype providing `Send` and `Sync` implementations around a [`JITModule`],
 /// and ensure that JIT memory gets freed at the correct time.
 #[derive(Debug)]
-pub(crate) struct JitModule(MaybeUninit<Mutex<JITModule>>);
+pub(crate) struct JitModule(MaybeUninit<JITModule>);
 
 impl std::ops::Deref for JitModule {
-	type Target = Mutex<JITModule>;
+	type Target = JITModule;
 
 	fn deref(&self) -> &Self::Target {
 		// SAFETY: The `JITModule` within only goes uninitialized when this type's
@@ -21,13 +20,18 @@ impl std::ops::Deref for JitModule {
 	}
 }
 
+impl std::ops::DerefMut for JitModule {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		// SAFETY: Same as `Deref`.
+		unsafe { self.0.assume_init_mut() }
+	}
+}
+
 impl Drop for JitModule {
 	fn drop(&mut self) {
 		unsafe {
 			let definitely_init = std::mem::replace(&mut self.0, MaybeUninit::uninit());
-			let mutex = definitely_init.assume_init();
-			let module = mutex.into_inner();
-			module.free_memory();
+			definitely_init.assume_init().free_memory();
 		}
 	}
 }
