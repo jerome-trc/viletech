4f8b55f66b03d01c27c55c66bce396a83ac6a223
jerome-trc <circus.of.rats@outlook.com>
2023-10-21 01:15:13 -0400

    VTBench: merge sharded_slab.rs into conc_ds.rs

--- bench/benches/conc_ds.rs
+++ bench/benches/conc_ds.rs
@@ -3,8 +3,15 @@
 //! ...when under ideal, minimal-contention conditions.
 
 use append_only_vec::AppendOnlyVec;
+use sharded_slab::Slab;
 
-criterion::criterion_group!(benches, append_only_vec, boxcar);
+criterion::criterion_group!(
+	benches,
+	append_only_vec,
+	boxcar,
+	sharded_slab,
+	sharded_slab_pool
+);
 criterion::criterion_main!(benches);
 
 fn append_only_vec(crit: &mut criterion::Criterion) {
@@ -54,6 +61,77 @@ fn boxcar(crit: &mut criterion::Criterion) {
 		bencher.iter(|| {
 			let _ = std::hint::black_box(aov[i]);
 		});
+	});
+
+	grp.finish();
+}
+
+fn sharded_slab(crit: &mut criterion::Criterion) {
+	let mut grp = crit.benchmark_group("sharded_slab::Slab");
+
+	grp.bench_function("Insert", |bencher| {
+		bencher.iter_batched_ref(
+			|| {
+				let slab = Slab::new();
+				let _ = slab.insert("warming it up first".to_string()).unwrap();
+				(slab, "???".to_string())
+			},
+			|(slab, string)| {
+				let _ = std::hint::black_box(slab.insert(std::mem::take(string)).unwrap());
+			},
+			criterion::BatchSize::SmallInput,
+		);
+	});
+
+	grp.bench_function("Access", |bencher| {
+		bencher.iter_batched_ref(
+			|| {
+				let slab = Slab::new();
+				let ix = slab.insert("???".to_string()).unwrap();
+				let _ = slab.insert("!!!".to_string()).unwrap();
+				let _ = slab.get(ix).unwrap();
+				(slab, ix)
+			},
+			|(slab, ix)| {
+				let _ = std::hint::black_box(slab.get(*ix).unwrap());
+			},
+			criterion::BatchSize::SmallInput,
+		);
+	});
+
+	grp.finish();
+}
+
+fn sharded_slab_pool(crit: &mut criterion::Criterion) {
+	let mut grp = crit.benchmark_group("sharded_slab::Pool");
+
+	grp.bench_function("Insert", |bencher| {
+		bencher.iter_batched_ref(
+			|| {
+				let pool = sharded_slab::Pool::<String>::new();
+				let _ = pool.create();
+				pool
+			},
+			|pool| {
+				let r = pool.create_with(|_| {});
+				let _ = std::hint::black_box(r.unwrap());
+			},
+			criterion::BatchSize::SmallInput,
+		);
+	});
+
+	grp.bench_function("Access", |bencher| {
+		bencher.iter_batched_ref(
+			|| {
+				let pool = sharded_slab::Pool::<String>::new();
+				let ix = pool.create_with(|s| s.push_str("???"));
+				(pool, ix.unwrap())
+			},
+			|(pool, ix)| {
+				let _ = std::hint::black_box(pool.get(*ix).unwrap());
+			},
+			criterion::BatchSize::SmallInput,
+		);
 	});
 
 	grp.finish();
