a87e8426c7cbf7637a4b2361ed60a9568fefdd5a
jerome-trc <circus.of.rats@outlook.com>
2023-10-24 09:06:50 -0400

    Lith: public functions on `rti::DataObj` to clear rustc warnings

--- lith/src/rti/data.rs
+++ lith/src/rti/data.rs
@@ -4,8 +4,25 @@ use cranelift_module::DataId;
 
 #[derive(Debug)]
 pub struct DataObj {
-	pub(crate) ptr: *const u8,
-	pub(crate) size: usize,
+	pub(crate) _ptr: *const u8,
 	pub(crate) id: DataId,
+	pub(crate) size: usize,
 	pub(crate) immutable: bool,
+}
+
+impl DataObj {
+	#[must_use]
+	pub fn id(&self) -> DataId {
+		self.id
+	}
+
+	#[must_use]
+	pub fn size(&self) -> usize {
+		self.size
+	}
+
+	#[must_use]
+	pub fn is_immutable(&self) -> bool {
+		self.immutable
+	}
 }
