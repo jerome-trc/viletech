9acf9f4b6ebb8ae56efaa0348eefeb80e1778882
jerome-trc <circus.of.rats@outlook.com>
2023-12-20 14:53:35 -0500

    Lith: replace Cranelift with MIR (for now)
    - It will be easier to get the language moving by using C2MIR as a backend.
    - Compile-time execution of LoLith functions falls out of MIR interpretation.
    - I tested Saltwater CC (which is Cranelift-based) on a Sieve of Eratosthenes, and it generated slower code than MIR's JIT. Unless there are other factors at play, there's at least a possibility that MIR will lead to higher script performance.

--- lith/benches/bench.rs
+++ lith/benches/bench.rs
@@ -1,20 +1,4 @@
-criterion::criterion_group!(benches, cranelift_ops);
+criterion::criterion_group!(benches, mir_ops);
 criterion::criterion_main!(benches);
 
-fn cranelift_ops(crit: &mut criterion::Criterion) {
-	let mut grp = crit.benchmark_group("Cranelift Ops");
-
-	grp.bench_function("New CLIF Function", |bencher| {
-		bencher.iter(|| {
-			let _ = std::hint::black_box(cranelift::codegen::ir::Function::new());
-		});
-	});
-
-	grp.bench_function("New CLIF Function Context", |bencher| {
-		bencher.iter(|| {
-			let _ = std::hint::black_box(cranelift::codegen::Context::new());
-		});
-	});
-
-	grp.finish();
-}
+fn mir_ops(_: &mut criterion::Criterion) {}
