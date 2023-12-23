03989c689a4022ba0a2dbfae6b417e1a6ac8fbdf
jerome-trc <circus.of.rats@outlook.com>
2023-12-22 20:38:57 -0500

    Engine: type alias `FxIndexSet`

--- engine/src/types.rs
+++ engine/src/types.rs
@@ -3,9 +3,11 @@
 use std::hash::BuildHasherDefault;
 
 use dashmap::DashMap;
-use indexmap::IndexMap;
+use indexmap::{IndexMap, IndexSet};
 use rustc_hash::FxHasher;
 
 pub type FxDashMap<K, V> = DashMap<K, V, BuildHasherDefault<FxHasher>>;
 pub type FxDashView<K, V> = dashmap::ReadOnlyView<K, V, BuildHasherDefault<FxHasher>>;
+
 pub type FxIndexMap<K, V> = IndexMap<K, V, BuildHasherDefault<FxHasher>>;
+pub type FxIndexSet<K> = IndexSet<K, BuildHasherDefault<FxHasher>>;
