8b51659cd4b81c6d0b8478bb1389be707d156dde
jerome-trc <circus.of.rats@outlook.com>
2023-12-28 19:11:54 -0500

    ZDBSP: rudimentary benchmark harness
    Not just for testing whether the Rust wrapper adds too much overhead, but also for testing where potential lies for performance improvement in the node-building routines themselves.

---
+++ zdbsp/benches/bench.rs
@@ -0,0 +1,32 @@
+use std::path::Path;
+
+use zdbsp::sys;
+
+criterion::criterion_group!(benches, end_to_end);
+criterion::criterion_main!(benches);
+
+fn end_to_end(crit: &mut criterion::Criterion) {
+	let mut grp = crit.benchmark_group("Wrapperless");
+
+	grp.sample_size(10);
+
+	let Ok(sample) = std::env::var("ZDBSP_SAMPLE") else {
+		eprintln!("Env. var. `ZDBSP_SAMPLE` not set; skipping benchmarks.");
+		return;
+	};
+
+	grp.bench_function("Vanilla", |bencher| {
+		let path = Path::new(&sample);
+		let bytes = std::fs::read(path).unwrap();
+
+		bencher.iter(|| unsafe {
+			let reader = sys::zdbsp_wadreader_new(bytes.as_ptr());
+			let p = sys::zdbsp_processor_new(reader, std::ptr::null());
+			sys::zdbsp_processor_run(p, std::ptr::null());
+			sys::zdbsp_processor_destroy(p);
+			sys::zdbsp_wadreader_destroy(reader);
+		});
+	});
+
+	grp.finish();
+}
