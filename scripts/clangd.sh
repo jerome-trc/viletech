82c5e039ed699fc7ee91c0cbded8668f3cc4ab30
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-05-07 07:53:17 -0400

    Script for generating .clangd file in `/nimcache`
    Inspecting Nim's generated code can occasionally be useful (particularly for understanding FFI behaviour) and this makes clangd able to help.

---
+++ scripts/clangd.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Generates a .clangd file for `/nimcache` so that clangd can be used when
+# inspecting generated C/C++ files.
+
+NIM_DIR=`choosenim show path`
+
+echo "---" > ./nimcache/.clangd
+echo "CompileFlags:" >> ./nimcache/.clangd
+echo "  Add: [" >> ./nimcache/.clangd
+echo "    '-isystem$NIM_DIR/lib'," >> ./nimcache/.clangd
+echo "    '-isystem../depend/daslang/include'," >> ./nimcache/.clangd
+echo "  ]" >> ./nimcache/.clangd
+echo "..." >> ./nimcache/.clangd
