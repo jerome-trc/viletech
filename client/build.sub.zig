867b3756739e5d565be9f67d176bf33c3a8ae23d
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-07-07 09:29:28 -0400

    Initial commit for new branch `ratdoom/zig`
    Zig curiosity strikes again. RatDoom is the next phase of this project, assembling pieces of other related source ports to arrive at my ideal Doom engine.
    
    Rust and Nim are other choices for languages to use, if Zig proves to be the wrong tool.

---
+++ client/build.sub.zig
@@ -0,0 +1,62 @@
+const std = @import("std");
+
+pub fn build(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    test_step: *std.Build.Step,
+) void {
+    const exe = b.addExecutable(.{
+        .name = "viletech",
+        .root_source_file = b.path("client/src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const exe_check = b.addExecutable(.{
+        .name = "viletech",
+        .root_source_file = b.path("client/src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const check = b.step("check", "Semantic check for ZLS");
+    check.dependOn(&exe_check.step);
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("client/src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
