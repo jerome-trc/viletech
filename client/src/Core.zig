3d704811b79bd93f41fe35dc3a8054ae9a2c3b1d
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-07-11 15:58:39 -0400

    Begin porting dsda-doom's (PrBoom's) PRNG

--- client/src/Core.zig
+++ client/src/Core.zig
@@ -1,9 +1,61 @@
 const std = @import("std");
 
+const Prng = @import("Prng.zig");
 const zdfs = @import("zdfs.zig");
 
 const Self = @This();
 const StreamWriter = std.io.BufferedWriter(4096, std.fs.File.Writer);
+
+pub const GameTick = i32;
+
+pub const Compat = enum {
+    doomV1p2,
+    doomV1p666,
+    doom2V1p9,
+    ultDoom,
+    finalDoom,
+    dosDoom,
+    tasDoom,
+    boomCompat,
+    boomV2p01,
+    boomV2p02,
+    lxDoom1,
+    mbf,
+    prboom1,
+    prboom2,
+    prboom3,
+    prboom4,
+    prboom5,
+    prboom6,
+    placeholder18,
+    placeholder19,
+    placeholder20,
+    mbf21,
+
+    const boom = @This().boomV2p01;
+    const best = @This().mbf21;
+};
+
+/// An untagged union is used here on the assumption that the engine is compiled
+/// in ReleaseSafe mode. Thus most code can easily get at any field without worrying
+/// about UB, while hot paths can locally disable safety.
+pub const Scene = union {
+    frontend: struct {
+        // ???
+    },
+    /// Includes menus.
+    game: struct {
+        compat: Compat,
+        game_tick: GameTick,
+        boom_basetick: GameTick,
+        true_basetick: GameTick,
+        /// In game ticks.
+        level_time: GameTick,
+        /// Sum of intermission times in game ticks at second resolution.
+        level_times_total: GameTick,
+        prng: Prng,
+    },
+};
 
 allo: std.mem.Allocator,
 fs: zdfs.VirtualFs,
@@ -12,6 +64,8 @@ stderr_file: std.fs.File.Writer,
 stderr_bw: StreamWriter,
 stdout_file: std.fs.File.Writer,
 stdout_bw: StreamWriter,
+
+scene: Scene,
 
 pub fn init() !Self {
     const stderr_file = std.io.getStdErr().writer();
@@ -24,6 +78,7 @@ pub fn init() !Self {
         .stderr_bw = std.io.bufferedWriter(stderr_file),
         .stdout_file = stdout_file,
         .stdout_bw = std.io.bufferedWriter(stdout_file),
+        .scene = Scene{ .frontend = .{} },
     };
 }
 
@@ -42,4 +97,16 @@ pub fn eprintln(self: *Self, comptime format: []const u8, args: anytype) !void {
 pub fn println(self: *Self, comptime format: []const u8, args: anytype) !void {
     try self.stdout_bw.writer().print(format ++ "\n", args);
     try self.stdout_bw.flush();
+}
+
+pub fn boom_compat(self: *const Self) bool {
+    return @intFromEnum(self.scene.game.compat) <= @intFromEnum(Compat.boomCompat);
+}
+
+pub fn demo_compat(self: *const Self) bool {
+    return @intFromEnum(self.scene.game.compat) < @intFromEnum(Compat.boomCompat);
+}
+
+pub fn boom_logictic(self: *const Self) GameTick {
+    self.scene.game.game_tick - self.scene.game.boom_basetick;
 }
