a2f92af4efd0d3b82c37782e72c3dadb34a1a206
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-07-11 14:27:53 -0400

    ZDFS catch-up: pass a message callback to `zdfs_fs_new`

--- client/src/zdfs.zig
+++ client/src/zdfs.zig
@@ -1,6 +1,7 @@
 //! Wrapper around ZDFS' C API for more ergonomic use by Zig code.
 
 const std = @import("std");
+const log = std.log.scoped(.zdfs);
 
 const c = @import("root").c;
 
@@ -11,7 +12,7 @@ pub const VirtualFs = packed struct {
 
     pub fn init() !Self {
         return Self{
-            .inner = c.zdfs_fs_new() orelse return error.FileSysInitNull,
+            .inner = c.zdfs_fs_new(messageCallback) orelse return error.FileSysInitNull,
         };
     }
 
@@ -23,3 +24,45 @@ pub const VirtualFs = packed struct {
 pub const Error = error{
     FileSysInitNull,
 };
+
+extern "C" fn snprintf(buffer: [*c]u8, bufsz: usize, format: [*c]const u8, ...) c_int;
+
+threadlocal var msgCbBuf = [_]u8{0} ** 1024;
+
+fn messageCallback(level: c.zdfs_MessageLevel, fmt: [*c]const u8, ...) callconv(.C) c_int {
+    var args = @cVaStart();
+    defer @cVaEnd(&args);
+
+    const written = snprintf(&msgCbBuf, msgCbBuf.len, fmt, args);
+
+    if (written < 0) {
+        log.err("ZDFS message callback failed due to encoding error.", .{});
+        return 0;
+    }
+
+    switch (level) {
+        c.zdfs_msglevel_error => {
+            log.err("{s}", .{&msgCbBuf});
+        },
+        c.zdfs_msglevel_warning => {
+            log.warn("{s}", .{&msgCbBuf});
+        },
+        c.zdfs_msglevel_attention => {
+            log.info("{s}", .{&msgCbBuf});
+        },
+        c.zdfs_msglevel_message => {
+            log.info("{s}", .{&msgCbBuf});
+        },
+        c.zdfs_msglevel_debugwarn => {
+            log.debug("{s}", .{&msgCbBuf});
+        },
+        c.zdfs_msglevel_debugnotify => {
+            log.debug("{s}", .{&msgCbBuf});
+        },
+        else => {
+            return 0;
+        },
+    }
+
+    return written;
+}
