abedad3608ae54bef06111fd26a600a6a1149826
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-07-17 10:33:12 -0400

    Roll (what will be) mus2mid into (what will be) Subterra

--- build.zig
+++ build.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 const client = @import("client/build.sub.zig");
-const mus2mid = @import("libs/mus2mid/build.sub.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
@@ -12,7 +11,7 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
 
-    mus2mid.build(b, target, optimize, test_step);
+    @import("libs/subterra/build.sub.zig").build(b, target, optimize, test_step);
 
     const engine = client.build(b, target, optimize, test_step);
 
---
+++ libs/subterra/build.sub.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+
+pub fn build(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    test_step: *std.Build.Step,
+) void {
+    const lib = b.addStaticLibrary(.{
+        .name = "subterra",
+        .root_source_file = b.path("libs/subterra/src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(lib);
+
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("libs/subterra/src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    test_step.dependOn(&run_lib_unit_tests.step);
+}
