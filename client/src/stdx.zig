e6c3f71db818105f5e8c93b8ec5be674b53b0b6e
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-07-20 10:21:12 -0400

    `stdx.asciiId`

--- client/src/stdx.zig
+++ client/src/stdx.zig
@@ -1,4 +1,25 @@
 //! Symbols which could reasonably be a part of the standard library.
 
+const builtin = @import("builtin");
+const std = @import("std");
+
 /// Provides disambiguation.
 pub const Path = [:0]const u8;
+
+/// Packs 4 `u8`s into one `u32`. For use in checking magic numbers.
+pub fn asciiId(b0: u8, b1: u8, b2: u8, b3: u8) u32 {
+    const b0_32: u32 = b0;
+    const b1_32: u32 = b1;
+    const b2_32: u32 = b2;
+    const b3_32: u32 = b3;
+
+    return if (builtin.cpu.arch.endian() == .little)
+        (b0_32 | (b1_32 << 8) | (b2_32 << 16) | (b3_32 << 24))
+    else
+        (b3_32 | (b2_32 << 8) | (b1_32 << 16) | (b0_32 << 24));
+}
+
+test "asciiId" {
+    const id = asciiId('M', 'T', 'h', 'd');
+    try std.testing.expectEqual(1684558925, id);
+}
