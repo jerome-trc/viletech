71ca4d50c4a9f483b51ae8f5265ebcceb15580d1
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-07-26 14:43:52 -0400

    Expanding the demo regression test suite

--- demotest/main.zig
+++ demotest/main.zig
@@ -1,5 +1,171 @@
 const std = @import("std");
 
+// Analysis ////////////////////////////////////////////////////////////////////
+
+test "doom2 map 4 nm speed by Vile" {
+    try runDemo(.{ .demo = "nm04-036.lmp" });
+    try expectAnalysis("skill", "5");
+    try expectAnalysis("respawn", "0");
+    try expectAnalysis("fast", "0");
+}
+
+test "doom2 map 1 uv speed by Thomas Pilger" {
+    try runDemo(.{ .demo = "lv01-005.lmp" });
+    try expectAnalysis("skill", "4");
+    try expectAnalysis("nomonsters", "0");
+    try expectAnalysis("100k", "0");
+    try expectAnalysis("100s", "0");
+}
+
+test "doom2 map 1 nomonsters by depr4vity" {
+    try runDemo(.{ .demo = "lv01o497.lmp" });
+    try expectAnalysis("nomonsters", "1");
+}
+
+test "doom2 map 2 uv respawn by Looper" {
+    try runDemo(.{ .demo = "re02-107.lmp" });
+    try expectAnalysis("respawn", "1");
+}
+
+test "doom2 map 4 uv fast by Radek Pecka" {
+    try runDemo(.{ .demo = "fa04-109.lmp" });
+    try expectAnalysis("fast", "1");
+}
+
+test "doom2 map 1 uv max by Xit Vono" {
+    try runDemo(.{ .demo = "lv01-039.lmp" });
+    try expectAnalysis("100k", "1");
+    try expectAnalysis("tyson_weapons", "0");
+    try expectAnalysis("turbo", "0");
+}
+
+test "doom2 episode 1 nm100s in 11:56 by JCD" {
+    try runDemo(.{ .demo = "1156ns01.lmp" });
+    try expectAnalysis("100s", "1");
+}
+
+test "doom2 ep 3 max in 26:54 by Vile" {
+    try runDemo(.{ .demo = "lve3-2654.lmp" });
+    try expectAnalysis("missed_monsters", "0");
+    try expectAnalysis("missed_secrets", "1");
+}
+
+test "pacifist, barrel chain" {
+    try runDemo(.{ .pwad = "analysis_test.wad", .demo = "barrel_chain.lmp" });
+    try expectAnalysis("pacifist", "0");
+}
+
+test "pacifist, barrel assist" {
+    try runDemo(.{ .pwad = "analysis_test.wad", .demo = "barrel_assist.lmp" });
+    try expectAnalysis("pacifist", "0");
+}
+
+test "pacifist, player shoots Keen" {
+    try runDemo(.{ .pwad = "analysis_test.wad", .demo = "keen.lmp" });
+    try expectAnalysis("pacifist", "0");
+}
+
+test "pacifist, player shoots Romero" {
+    try runDemo(.{ .pwad = "analysis_test.wad", .demo = "romero.lmp" });
+    try expectAnalysis("pacifist", "0");
+}
+
+test "pacifist, splash damage" {
+    try runDemo(.{ .pwad = "analysis_test.wad", .demo = "splash.lmp" });
+    try expectAnalysis("pacifist", "0");
+}
+
+test "pacifist, telefrag" {
+    try runDemo(.{ .pwad = "analysis_test.wad", .demo = "telefrag.lmp" });
+    try expectAnalysis("pacifist", "1");
+}
+
+test "pacifist, player shoots voodoo doll" {
+    try runDemo(.{ .pwad = "analysis_test.wad", .demo = "voodoo.lmp" });
+    try expectAnalysis("pacifist", "1");
+}
+
+test "doom2 map 8 stroller by 4shockblast" {
+    try runDemo(.{ .demo = "lv08str037.lmp" });
+    try expectAnalysis("stroller", "1");
+}
+
+test "doom2 map 8 pacifist by 4shockblast" {
+    try runDemo(.{ .demo = "pa08-020.lmp" });
+    try expectAnalysis("stroller", "0");
+}
+
+test "reality, player takes enemy damage" {
+    try runDemo(.{ .pwad = "analysis_test.wad", .demo = "damage.lmp" });
+    try expectAnalysis("reality", "0");
+    try expectAnalysis("almost_reality", "0");
+}
+
+test "reality, player takes nukage damage" {
+    try runDemo(.{ .pwad = "analysis_test.wad", .demo = "nukage.lmp" });
+    try expectAnalysis("reality", "0");
+    try expectAnalysis("almost_reality", "1");
+}
+
+test "reality, player takes crusher damage" {
+    try runDemo(.{ .pwad = "analysis_test.wad", .demo = "crusher.lmp" });
+    try expectAnalysis("reality", "0");
+    try expectAnalysis("almost_reality", "0");
+}
+
+test "reality, player takes no damage" {
+    try runDemo(.{ .pwad = "analysis_test.wad", .demo = "reality.lmp" });
+    try expectAnalysis("reality", "1");
+    try expectAnalysis("almost_reality", "0");
+}
+
+test "doom2 map 1 tyson by j4rio" {
+    try runDemo(.{ .demo = "lv01t040.lmp" });
+    try expectAnalysis("tyson_weapons", "1");
+    try expectAnalysis("weapon_collector", "0");
+}
+
+test "doom2 done turbo quicker by 4shockblast" {
+    try runDemo(.{ .demo = "d2dtqr.lmp" });
+    try expectAnalysis("turbo", "1");
+}
+
+test "doom2 map 1 collector by hokis" {
+    try runDemo(.{ .demo = "cl01-022.lmp" });
+    try expectAnalysis("weapon_collector", "1");
+}
+
+// Sync ////////////////////////////////////////////////////////////////////////
+
+test "doom2 30uv in 17:55 by Looper" {
+    try runDemo(.{ .demo = "30uv1755.lmp" });
+    try expectTotalTime("17:55");
+}
+
+test "doom2 20 uv max in 2:22 by termrork & kOeGy (a)" {
+    try runDemo(.{ .demo = "cm20k222.lmp" });
+    try expectTotalTime("2:22");
+}
+
+test "doom2 20 uv max in 2:22 by termrork & kOeGy (b)" {
+    try runDemo(.{ .demo = "cm20t222.lmp" });
+    try expectTotalTime("2:22");
+}
+
+test "rush 12 uv max in 21:14 by Ancalagon" {
+    try runDemo(.{ .pwad = "rush.wad", .demo = "ru12-2114.lmp" });
+    try expectTotalTime("21:14");
+}
+
+test "valiant e1 uv speed in 5:13 by Krankdud" {
+    try runDemo(.{ .pwad = "Valiant.wad", .demo = "vae1-513.lmp" });
+    try expectTotalTime("5:13");
+}
+
+// TODO: Heretic and Hexen tests, after I've bought both.
+
+// Details /////////////////////////////////////////////////////////////////////
+
 const alloc = std.testing.allocator;
 
 fn runDemo(args: struct {
@@ -53,16 +219,6 @@ fn runDemo(args: struct {
 
     alloc.free(result.stdout);
     alloc.free(result.stderr);
-}
-
-test "nm04-036.lmp" {
-    try runDemo(.{ .demo = "nm04-036.lmp" });
-    try expectAnalysis("skill", "5");
-}
-
-test "vae1-513.lmp" {
-    try runDemo(.{ .pwad = "Valiant.wad", .demo = "vae1-513.lmp" });
-    try expectTotalTime("5:13");
 }
 
 fn expectAnalysis(key: []const u8, val: []const u8) !void {
