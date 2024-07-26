6be38e2d07e115bd37e8951e52e29e51ded9c89e
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-07-26 16:00:24 -0400

    Disable FLECS HTTP and REST

--- depend/build.flecs.zig
+++ depend/build.flecs.zig
@@ -3,7 +3,11 @@ const std = @import("std");
 pub fn link(b: *std.Build, compile: *std.Build.Step.Compile) void {
     compile.addCSourceFile(.{
         .file = b.path("depend/flecs/flecs.c"),
-        .flags = &[_][]const u8{"-std=gnu99"},
+        .flags = &[_][]const u8{
+            "-std=gnu99",
+            "-DFLECS_NO_HTTP",
+            "-DFLECS_NO_REST",
+        },
     });
 
     compile.addSystemIncludePath(b.path("depend/flecs"));
