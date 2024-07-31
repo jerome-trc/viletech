30e9cd29250863d436c8be317dfe005b56abe5aa
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-07-31 11:36:59 -0400

    Call all functions which create an ImGui window `layout`

--- client/src/devgui/VfsGui.zig
+++ client/src/devgui/VfsGui.zig
@@ -20,7 +20,7 @@ pub fn init() Self {
     };
 }
 
-pub fn draw(cx: *Core, left: bool, menu_bar_height: f32) void {
+pub fn layout(cx: *Core, left: bool, menu_bar_height: f32) void {
     var self = &cx.vfsgui;
 
     const vp_size = if (c.igGetMainViewport()) |vp| vp.*.Size else {
