930fb07387a8f4ea8692b873ef7ccfb9d7995f22
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-08-16 14:17:15 -0400

    Bring in Subterra from the `ratdoom/zig` branch
    From here on out, development of software which isn't strictly RatBoom-relevant will happen on this branch and then get moved to whatever branch I end up making the mainline.

---
+++ libs/subterra/build.subterra.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+const Context = @import("../../build.zig").Context;
+
+pub fn build(
+    b: *std.Build,
+    ctx: *const Context,
+) *std.Build.Module {
+    const mod = b.addModule("subterra", .{
+        .root_source_file = b.path("libs/subterra/src/root.zig"),
+    });
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("libs/subterra/src/root.zig"),
+        .target = ctx.target,
+        .optimize = ctx.optimize,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    ctx.test_step.dependOn(&run_unit_tests.step);
+
+    return mod;
+}
---
+++ libs/subterra/src/root.zig
@@ -0,0 +1,1 @@
+pub const mus = @import("mus.zig");
