69b177f45f8139ac9c2841c0110b5be2e4931dca
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-08-17 07:10:39 -0400

    Subterra: port LINEDEFS-reading functionality from Rust

--- libs/subterra/src/root.zig
+++ libs/subterra/src/root.zig
@@ -1,1 +1,15 @@
+pub const level = @import("level.zig");
 pub const mus = @import("mus.zig");
+
+pub const Game = enum {
+    boom, // Not a game per se...
+    doom,
+    doom64,
+    heretic,
+    hexen,
+    strife,
+};
+
+test {
+    @import("std").testing.refAllDecls(level);
+}
