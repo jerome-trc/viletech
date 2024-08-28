e13d3a84a05d0e6efdb44ba1c4476ca596d2d897
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-08-28 08:46:39 -0400

    `UNREACHABLE` macro in `viletech/stdx.h`

--- dsda-doom/prboom2/src/viletech/stdx.h
+++ dsda-doom/prboom2/src/viletech/stdx.h
@@ -6,6 +6,12 @@
 
 #if !defined(RATBOOM_ZIG) // If included by Zig, don't expand to anything.
 
+#define UNREACHABLE \
+    do { \
+        fprintf(stderr, "unreachable code: %s:%u", __FILE__, __LINE__); \
+        abort(); \
+    } while (0)
+
 /// The returned pointer is not null-terminated!
 const char* pathStem(const char* path, size_t* out_len);
 
