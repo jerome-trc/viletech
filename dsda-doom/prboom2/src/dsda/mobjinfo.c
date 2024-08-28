f876aa31664656e103d7d82feca8828730b4605f
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-08-28 08:48:13 -0400

    Fix UB in `dsda_GetDehMobjInfo`

--- dsda-doom/prboom2/src/dsda/mobjinfo.c
+++ dsda-doom/prboom2/src/dsda/mobjinfo.c
@@ -84,7 +84,12 @@ dsda_deh_mobjinfo_t dsda_GetDehMobjInfo(int index) {
   dsda_EnsureCapacity(index);
 
   deh_mobjinfo.info = &mobjinfo[index];
-  deh_mobjinfo.edited_bits = &edited_mobjinfo_bits[index];
+
+    if (edited_mobjinfo_bits == NULL) {
+        deh_mobjinfo.edited_bits = NULL;
+    } else {
+        deh_mobjinfo.edited_bits = &edited_mobjinfo_bits[index];
+    }
 
   return deh_mobjinfo;
 }
