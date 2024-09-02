032a0208a88c16d8854d08f2f439e2321643d07e
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-09-02 13:06:40 -0400

    Subterra: port VERTEXES-reading functionality from Rust

--- libs/subterra/src/level.zig
+++ libs/subterra/src/level.zig
@@ -499,6 +499,67 @@ pub fn Thing(game: Game) type {
     };
 }
 
+// VERTEXES ////////////////////////////////////////////////////////////////////
+
+pub const Vertex = extern struct {
+    x: i16,
+    y: i16,
+
+    /// Caller guarantees that `bytes.len` is divisible by `@sizeOf(@This())`.
+    pub fn fromBytes(bytes: []align(@alignOf(Vertex)) const u8) []const Vertex {
+        return std.mem.bytesAsSlice(Vertex, bytes);
+    }
+
+    /// Caller guarantees that `bytes.len` is divisible by `@sizeOf(@This())`.
+    pub fn fromBytesMut(bytes: []align(@alignOf(Vertex)) u8) []Vertex {
+        return std.mem.bytesAsSlice(Vertex, bytes);
+    }
+
+    pub fn fromBytesLossy(bytes: []align(@alignOf(Vertex)) const u8) []const Vertex {
+        const count = bytes.len / @sizeOf(Vertex);
+        return std.mem.bytesAsSlice(Vertex, bytes[0..(count * @sizeOf(Vertex))]);
+    }
+
+    pub fn fromBytesLossyMut(bytes: []align(@alignOf(Vertex)) u8) []Vertex {
+        const count = bytes.len / @sizeOf(Vertex);
+        return std.mem.bytesAsSlice(Vertex, bytes[0..(count * @sizeOf(Vertex))]);
+    }
+
+    pub fn bounds(verts: []const Vertex) Bounds {
+        var min = Position{ .x = 0, .y = 0 };
+        var max = Position{ .x = 0, .y = 0 };
+
+        for (verts) |vert| {
+            if (vert.x < min.x) {
+                min.x = vert.x;
+            } else if (vert.x > max.x) {
+                max.x = vert.x;
+            }
+
+            if (vert.y < min.y) {
+                min.y = vert.y;
+            } else if (vert.y > max.y) {
+                max.y = vert.y;
+            }
+        }
+
+        return Bounds{ .min = min, .max = max };
+    }
+
+    pub fn position(self: *const Vertex) Position {
+        return Position{
+            .x = std.mem.littleToNative(i16, self.x),
+            .y = std.mem.littleToNative(i16, self.y),
+        };
+    }
+};
+
+/// See [`Vertex.bounds`].
+pub const Bounds = struct {
+    min: Position,
+    max: Position,
+};
+
 // Unit tests //////////////////////////////////////////////////////////////////
 
 test "LINEDEFS, semantic check" {
@@ -643,4 +704,23 @@ test "THINGS, Hexen, fromBytes" {
     );
     try std.testing.expectEqual(80, things[0].actionSpecial());
     try std.testing.expectEqual([5]u8{ 14, 1, 0, 0, 0 }, things[0].args());
+}
+
+test "VERTEXES, fromBytes" {
+    const bytes = [8]u8{
+        // First vertex of E1M1...
+        0x40, 0x04, 0xA0, 0xF1,
+        // Second vertex of E1M1...
+        0x00, 0x04, 0xA0, 0xF1,
+    };
+    const vertexes = Vertex.fromBytes(@alignCast(bytes[0..]));
+
+    try std.testing.expectEqual(
+        Position{ .x = 1088, .y = -3680 },
+        vertexes[0].position(),
+    );
+    try std.testing.expectEqual(
+        Position{ .x = 1024, .y = -3680 },
+        vertexes[1].position(),
+    );
 }
