ca436af91082a535128af46b65aaf013532e2508
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-09-04 18:24:00 -0400

    E: `BoomRng.zig` from the `ratdoom/zig` branch

--- engine/src/root.zig
+++ engine/src/root.zig
@@ -1,4 +1,5 @@
 //! A Zig library that fills in the gaps between the other libraries in the
 //! VileTech project for consumption by the client.
 
+pub const BoomRng = @import("BoomRng.zig");
 pub const stdx = @import("stdx.zig");
