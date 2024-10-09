cb9a7d930e157a9a7a8c3019ebe65178d5d412b1
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-10-09 18:09:33 -0400

    DoomParse: recursively reference root during testing

--- libs/doomparse/src/root.zig
+++ libs/doomparse/src/root.zig
@@ -1,3 +1,7 @@
 //! Parsers for all manner of languages in the id Tech 1-descendant ecosystem.
 
 pub const doomtools = @import("doomtools.zig");
+
+test {
+    @import("std").testing.refAllDeclsRecursive(@This());
+}
