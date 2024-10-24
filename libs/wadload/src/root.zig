902e4ca47cad5af9997bf4289fd0b157c62cda8d
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-10-24 14:41:39 -0400

    WADLoad: fix a stray character in root doc comment

--- libs/wadload/src/root.zig
+++ libs/wadload/src/root.zig
@@ -1,6 +1,6 @@
 //! # WADLoad
 //!
-//! Simple, pay-only-for-what-you-use I/O for archive formats from old video games.a
+//! Simple, pay-only-for-what-you-use I/O for archive formats from old video games.
 
 pub const wad = @import("wad.zig");
 
